30d7c854636c1d6dedd008424f4952efb10c1728
theetom <tomazf4@gmail.com>
2025-04-02 14:12:01 +0100

    teste mas nn funciona

--- teste/main.c
+++ teste/main.c
@@ -22,50 +22,196 @@ typedef struct s_data
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+
+	double posX, posY;  //x and y start position
+	double dirX, dirY; //initial direction vector
+	double planeX, planeY; //the 2d raycaster version of camera plane
+  
+	double time; //time of current frame
+	double oldTime; //time of previous frame
+
 }	t_data;
 
 int worldMap[mapWidth][mapHeight]=
 {
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,0,1,0,1,0,0,0,1},
-  {1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,1},
-  {1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,1,0,1,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+  };
+
+typedef struct {
+    unsigned char r, g, b;
+} ColorRGB;
+
+// Define color constants
+const ColorRGB RGB_Red    = {255, 0, 0};
+const ColorRGB RGB_Green  = {0, 255, 0};
+const ColorRGB RGB_Blue   = {0, 0, 255};
+const ColorRGB RGB_White  = {255, 255, 255};
+const ColorRGB RGB_Yellow = {255, 255, 0};
+
+// Function to reduce brightness
+ColorRGB dimColor(ColorRGB color)
+{
+    color.r /= 2;
+    color.g /= 2;
+    color.b /= 2;
+    return color;
+}
+int color_from_c_to_int(ColorRGB color)
+{
+	return(color.r << 24| color.g << 16 | color.b << 8 | 100);
+}
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void create_image(t_data *data)
+{
+	int x = 0;
+
+	while (x < data->img_w)
+	{
+		double cameraX = 2 * x / data->img_w - 1; //x-coordinate in camera space
+		double rayDirX = data->dirX + data->planeX * cameraX;
+		double rayDirY = data->dirY + data->planeY * cameraX;
+		int mapX = (int)data->posX;
+		int mapY = (int)data->posY;
+		double sideDistX;
+		double sideDistY;
+		double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1.0 / rayDirX);
+		double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1.0 / rayDirY);
+		double perpWallDist;
+
+		//what direction to step in x or y-direction (either +1 or -1)
+		int stepX;
+		int stepY;
+  
+		int hit = 0; //was there a wall hit?
+		int side; //was a NS or a EW wall hit?
+		//calculate step and initial sideDist
+		if(rayDirX < 0)
+		{
+			stepX = -1;
+			sideDistX = (data->posX - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - data->posX) * deltaDistX;
+		}
+		if(rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (data->posY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - data->posY) * deltaDistY;
+		}
+		//perform DDA
+		while(hit == 0)
+		{
+		//jump to next map square, either in x-direction, or in y-direction
+			if(sideDistX < sideDistY)
+			{
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			//Check if ray has hit a wall
+			if(worldMap[mapX][mapY] > 0)
+				hit = 1;
+		}
+		if(side == 0)
+			perpWallDist = (sideDistX - deltaDistX);
+		else
+			perpWallDist = (sideDistY - deltaDistY);
+		int lineHeight = (int)(data->img_h / perpWallDist);
+		//calculate lowest and highest pixel to fill in current stripe
+		int drawStart = -lineHeight / 2 + data->img_h / 2;
+		if(drawStart < 0)
+			drawStart = 0;
+		int drawEnd = lineHeight / 2 + data->img_h / 2;
+		if(drawEnd >= data->img_h)
+			drawEnd = data->img_h - 1;
+		ColorRGB color;
+		switch(worldMap[mapX][mapY])
+		{
+			case 1:  color = RGB_Red;    break;
+			case 2:  color = RGB_Green;  break;
+			case 3:  color = RGB_Blue;   break;
+			case 4:  color = RGB_White;  break;
+			default: color = RGB_Yellow; break;
+		}
+	
+		// Give x and y sides different brightness
+		if (side == 1) {
+			color = dimColor(color);
+		}
+		int y = drawStart;
+		while (y < drawEnd)
+		{
+			printf("%d\n", color_from_c_to_int(color));
+			my_mlx_pixel_put(data, x, y++, color_from_c_to_int(color));
+		}
+		printf("ola\n");
+		x++;
+	}
+	printf("olaboa tarde\n");
+}
 
 static void	render(t_data *data)
 {
 	mlx_destroy_image(data->mlx, data->img);
 	data->img = mlx_new_image(data->mlx, data->img_w, data->img_h);
+	create_image(data);
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img, 0, 0);
 }
 
-static void mlx_data_init(t_data *data)
+int delete_everything(t_data *data)
 {
-	data->mlx = mlx_init();
-	data->mlx_win = mlx_new_window(data->mlx, data->img_w, data->img_h, "My CUB3D!");
-	ft_hooks(&data);
-	data->img = mlx_new_image(data->mlx, data->img_w, data->img_h);
-	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, &data->line_length, &data->endian);
+	(void)data;
+	exit(0);
+}
+
+int	keypress(int k, t_data *data)
+{ 
+	if (k == 0xff1b)
+		delete_everything(data);
 }
 
 void	ft_hooks(t_data *data)
@@ -73,33 +219,30 @@ void	ft_hooks(t_data *data)
 	mlx_hook(data->mlx_win, 02, (1L << 0), keypress, data);
 	mlx_hook(data->mlx_win, 17, 1L << 17, delete_everything, data);
 }
-
-void delete_everything(t_data *data)
+static void mlx_data_init(t_data *data)
 {
-	(void)data;
-	exit(0);
-}
-
-int	keypress(int k, t_data *data)
-{
-	if (k == 0xff1b)
-		delete_everything(data);
+	data->mlx = mlx_init();
+	data->mlx_win = mlx_new_window(data->mlx, data->img_w, data->img_h, "My CUB3D!");
+	ft_hooks(data);
+	data->img = mlx_new_image(data->mlx, data->img_w, data->img_h);
+	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, &data->line_length, &data->endian);
 }
 
 int main(int argc, char *argv[])
 {
 	t_data	data;
 
-	data.img_h = 1280;
-	data.img_w = 1024;
-	double posX = 22, posY = 12;  //x and y start position
-	double dirX = -1, dirY = 0; //initial direction vector
-	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+	data.img_h = 480;
+	data.img_w = 640;
+	data.posX = 22, data.posY = 12;  //x and y start position
+	data.dirX = -1, data.dirY = 0; //initial direction vector
+	data.planeX = 0, data.planeY = 0.66; //the 2d raycaster version of camera plane
   
-	double time = 0; //time of current frame
-	double oldTime = 0; //time of previous frame
+	data.time = 0; //time of current frame
+	data.oldTime = 0; //time of previous frame
 
 	mlx_data_init(&data);
+	create_image(&data);
 	mlx_put_image_to_window(data.mlx, data.mlx_win, data.img, 0, 0);
 	mlx_loop(data.mlx);
 }
