d3e9b4d8b61601bbc8a3f99d95005a91a77c8f40
Tomaz Ferreira De Lima <toferrei@c1r9s13.42lisboa.com>
2025-05-26 14:47:40 +0100

    colision corrected

--- Sources/ft_move_up_down.c
+++ Sources/ft_move_up_down.c
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 16:00:16 by toferrei          #+#    #+#             */
-/*   Updated: 2025/05/23 10:07:14 by toferrei         ###   ########.fr       */
+/*   Updated: 2025/05/26 14:34:41 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ void	ft_move_forward(t_data *data)
 
 	max_x = data->map->max_x;
 	max_y = data->map->max_y;
-	if (0 < (data->pos_x + data->dir_x) \
-	&& max_x > (data->pos_x + data->dir_x))
+	if (0 < (data->pos_x + data->dir_x * data->move_speed * data->speed) \
+	&& max_x > (data->pos_x + data->dir_x * data->move_speed * data->speed))
 	{
 		if (data->world_map[(int)data->pos_y] \
-		[(int)(data->pos_x + data->dir_x)] == 0)
-			data->pos_x += (data->dir_x * data->move_speed) * data->speed;
+		[(int)(data->pos_x + data->dir_x * data->move_speed * data->speed)] == 0)
+			data->pos_x += data->dir_x * data->move_speed * data->speed;
 	}
-	if (0 < (data->pos_y + data->dir_y) \
-		&& max_y > (data->pos_y + data->dir_y))
+	if (0 < (data->pos_y + data->dir_y * data->move_speed * data->speed) \
+		&& max_y > (data->pos_y + data->dir_y * data->move_speed * data->speed))
 	{
-		if (data->world_map[(int)(data->pos_y + data->dir_y)] \
+		if (data->world_map[(int)(data->pos_y + data->dir_y * data->move_speed * data->speed)] \
 		[(int)(data->pos_x)] == 0)
-			data->pos_y += (data->dir_y * data->move_speed) * data->speed;
+			data->pos_y += data->dir_y * data->move_speed * data->speed;
 	}
 }
 
@@ -42,18 +42,18 @@ void	ft_move_backward(t_data *data)
 
 	max_x = data->map->max_x;
 	max_y = data->map->max_y;
-	if (0 < (data->pos_x + data->dir_x) \
-	&& max_x > (data->pos_x + data->dir_x))
+	if (0 < (data->pos_x + data->dir_x * data->move_speed * data->speed) \
+	&& max_x > (data->pos_x + data->dir_x * data->move_speed * data->speed))
 	{
 		if (data->world_map[(int)data->pos_y] \
-		[(int)(data->pos_x - data->dir_x)] == 0)
-			data->pos_x -= (data->dir_x * data->move_speed) * data->speed;
+		[(int)(data->pos_x - data->dir_x * data->move_speed * data->speed)] == 0)
+			data->pos_x -= data->dir_x * data->move_speed * data->speed;
 	}
-	if (0 < (data->pos_y + data->dir_y) \
-	&& max_y > (data->pos_y + data->dir_y))
+	if (0 < (data->pos_y + data->dir_y * data->move_speed * data->speed) \
+	&& max_y > (data->pos_y + data->dir_y * data->move_speed * data->speed))
 	{
-		if (data->world_map[(int)(data->pos_y - data->dir_y)] \
+		if (data->world_map[(int)(data->pos_y - data->dir_y * data->move_speed * data->speed)] \
 		[(int)(data->pos_x)] == 0)
-			data->pos_y -= (data->dir_y * data->move_speed * data->speed);
+			data->pos_y -= data->dir_y * data->move_speed * data->speed;
 	}
 }
